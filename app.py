ed3485cd1b315f967bca56ebd23afb12b5d99b17
artem <artem25101996@gmail.com>
2025-10-27 17:18:22 +0300

    сохранение данных

--- app.py
+++ app.py
@@ -1435,5 +1435,4 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,passthrough_errors=True,
-    use_debugger=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
